5669ba972a45d75db9090b5c757b858a91fa4b52
petermikitsh <peter.mikitsh@gmail.com>
2013-12-03 21:49:29 -0500

    left navbar; dynamically load projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :ldap_authenticatable, :rememberable, :trackable
+
+  has_and_belongs_to_many :projects
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131204014827) do
+ActiveRecord::Schema.define(version: 20131204020534) do
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects_users", id: false, force: true do |t|
+    t.integer "project_id"
+    t.integer "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "login",               default: "", null: false
