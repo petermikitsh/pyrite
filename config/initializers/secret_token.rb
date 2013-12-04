bfbe9044ec5e32ba048bb7d61548fe0faab888d5
petermikitsh <peter.mikitsh@gmail.com>
2013-12-03 20:27:29 -0500

    ruby 2 & rails 4 base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pyrite::Application.config.secret_key_base = 'd6f8c0bc9983bec8da28ddf94327ffb842f4e709250bd3b6e858c200e3f11cd6d48e7704efed48fae19946f2283b49a071e11cac8938fb7ffec8430a5b16b7f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pyrite::Application.config.session_store :cookie_store, key: '_pyrite_session'
